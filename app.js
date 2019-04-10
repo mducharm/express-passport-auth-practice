3405c30c54faa7a9968f97046ecb0782113e5933
Michael <sheveksmath@gmail.com>
2019-04-09 21:08:19 -0400

    added routes for signing up and logging in. checks DB to see if username exists for signup, and checks for matching user & pw for login

--- app.js
+++ app.js
@@ -4,12 +4,54 @@ const { User } = require("./sequelize");
 
 let app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static("public"));
 
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+
+  User.findOrCreate({
+    where: {
+      username: req.body.username
+    },
+    defaults: { username: req.body.username, password: req.body.password }
+  }).then(([user, created]) => {
+    let credentials = user.get({ plain: true });
+    // console.log(credentials.username);
+    // console.log(credentials.password);
+
+    if (!created) {
+      res.send("Sorry, username taken");
+    } else {
+      res.send(`Account created with username of ${credentials.username}`);
+    }
+  });
+
+  //   User.findOne().then(user => {
+  //       if (user) {
+  //           res.send(404)
+  //       } else {
+  //           User.create()
+  //       }
+  //   });
+});
+
 app.post("/login", (req, res) => {
-  User.findAll({ attributes: ["username", "password"] }).then(result => {
-    console.log(result);
-    res.send("potato");
+  //   console.log(req.body);
+  //   res.send("Logged in?");
+  User.findOne({
+    where: {
+      username: req.body.username,
+      password: req.body.password
+    }
+  }).then(user => {
+    if (user) {
+      res.send("Logged in!");
+    } else {
+      res.send("Not a valid login");
+    }
   });
 });
 
